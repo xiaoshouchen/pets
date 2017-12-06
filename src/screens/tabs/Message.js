87fd049073516a98b817783494133ff8c126fd61
xiaozhen <xiaozhen0801@hotmail.com>
2017-12-06 12:40:15 +0800

    更改依赖

--- src/screens/tabs/Message.js
+++ src/screens/tabs/Message.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import RichEditor from 'react-native-webview-richeditor';
+import {RichTextEditor, RichTextToolbar} from 'react-native-zss-rich-text-editor';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 
 const MessageScreen = ({ navigation }) => (
+  <View style={styles.container}>
   <RichEditor />  
+  <KeyboardSpacer/>
+  </View>
 )
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
 export {MessageScreen}
