d1c29ef9ee5adea11b84cb2db012be11e2a71ed3
缪国豪 <446362429@qq.com>
2018-01-30 14:59:21 +0800

    搜索修改

--- src/screens/tabs/Home/Search.js
+++ src/screens/tabs/Home/Search.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     Modal,
     StyleSheet, Text, TextInput, TouchableOpacity, View
 } from 'react-native';
 
-export class SearchModal extends Component{
+export class SearchModal extends Component {
 
     constructor(props) {
         super(props);
@@ -12,31 +12,45 @@ export class SearchModal extends Component{
             modalVisible: true
         }
     }
+
     setModalVisible(visible) {
         this.setState({modalVisible: visible});
     }
-    render(){
-        return(
+
+    render() {
+        return (
             <Modal
                 animationType={"slide"}
                 transparent={false}
-                visible={this.props.modalVisible == true? this.state.modalVisible:!this.state.modalVisible}
-                onRequestClose={() => {alert("Modal has been closed.")}}
+                visible={this.props.modalVisible == true ? this.state.modalVisible : !this.state.modalVisible}
+                onRequestClose={() => {
+                    alert("Modal has been closed.")
+                }}
             >
                 <View style={{marginTop: 22}}>
-                    <View style={{flexDirection: 'row', justifyContent: 'space-between',marginLeft: 15, marginRight: 15, alignItems: 'center'}}>
+                    <View style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        marginLeft: 10,
+                        marginRight: 10,
+                        alignItems: 'center',
+                        backgroundColor: 'white'
+                    }}>
                         <TextInput
                             underlineColorAndroid={'transparent'}
                             style={styles.input}
-                            placeholder ={'搜索'}
+                            placeholder={'搜索'}
                             onChangeText={(text) => this.setState({
                                 search: text
                             })}
                         />
                         <TouchableOpacity onPress={() => this.setModalVisible(!this.state.modalVisible)}>
-                            <Text>关闭</Text>
+                            <Text style={{width: 35, textAlign: 'center'}}>关闭</Text>
                         </TouchableOpacity>
 
+                    </View>
+                    <View style={{backgroundColor: '#F0F0F0', marginLeft: 10, marginTop: 20}}>
+                        <Text>搜索历史</Text>
                     </View>
                 </View>
             </Modal>
@@ -47,10 +61,11 @@ export class SearchModal extends Component{
 const styles = StyleSheet.create({
     input: {
         textAlign: 'left',
-        flex:1,
+        flex: 1,
         height: 40,
-        padding: 0,
-        backgroundColor: 'grey'
+        padding: 10,
+        backgroundColor: '#F0F0F0',
+        borderRadius: 20
     }
 })
 
