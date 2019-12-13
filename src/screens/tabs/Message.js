0840c1d1523bc9c8de7e75577c08d0bbaa9ba2cc
xiaozhen <859690389@qq.com>
2019-12-13 20:13:34 +0800

    修改

--- src/screens/tabs/Message.js
+++ src/screens/tabs/Message.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, ScrollView, Button, Image, TouchableOpacity, Modal} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView, Button, Image, TouchableOpacity, Modal } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import Dimensions from 'Dimensions'
 import StartRecord from '../../utils/StartRecord'
@@ -19,13 +19,13 @@ class MessageScreen extends Component {
         tabBarLabel: " ",
         showLabel: false,
         header: null,
-        tabBarIcon: ({focused}) => {
+        tabBarIcon: ({ focused }) => {
 
             if (!focused) {
-                return <Image source={require('../../image/plus.png')} style={{width: 48, height: 48}}/>
+                return <Image source={require('../../image/plus.png')} style={{ width: 48, height: 48 }} />
             }
             else {
-                return <Image source={require('../../image/plus-active.png')} style={{width: 48, height: 48}}/>
+                return <Image source={require('../../image/plus-active.png')} style={{ width: 48, height: 48 }} />
             }
         }
     }
@@ -35,7 +35,7 @@ class MessageScreen extends Component {
             <View style={styles.mainView}>
                 <View style={styles.topic}>
                     <View>
-                        <Text style={{fontSize: 24, marginVertical: 20}}>捏捏宠</Text>
+                        <Text style={{ fontSize: 24, marginVertical: 20 }}>捏捏宠</Text>
                     </View>
                     <View>
                         <Text>高质量的宠物社区</Text>
@@ -44,17 +44,17 @@ class MessageScreen extends Component {
                 <View style={styles.mainSection}>
                     <View style={styles.icons}>
                         <TouchableOpacity
-                            onPress={() => this.props.navigation.navigate('AddArticle', {type_id: 1})}>
+                            onPress={() => this.props.navigation.navigate('AddArticle', { type_id: 1 })}>
                             <View style={styles.main}>
-                                <Image style={styles.imageView} source={require('../../image/post/share.png')}/>
+                                <Image style={styles.imageView} source={require('../../image/post/share.png')} />
                                 <Text style={styles.nameView}>分享</Text>
                                 <Text style={styles.textView}>经验或故事</Text>
                             </View>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            onPress={() => this.props.navigation.navigate('AddArticle', {type_id: 2})}>
+                            onPress={() => this.props.navigation.navigate('AddArticle', { type_id: 2 })}>
                             <View style={styles.main}>
-                                <Image style={styles.imageView} source={require('../../image/post/ask.png')}/>
+                                <Image style={styles.imageView} source={require('../../image/post/ask.png')} />
                                 <Text style={styles.nameView}>提问</Text>
                                 <Text style={styles.textView}>养宠难题</Text>
                             </View>
@@ -68,9 +68,9 @@ class MessageScreen extends Component {
                         {/*</View>*/}
                         {/*</TouchableOpacity>*/}
                         <TouchableOpacity
-                            onPress={() => this.props.navigation.navigate('AddArticle', {type_id: 3})}>
+                            onPress={() => this.props.navigation.navigate('AddArticle', { type_id: 3 })}>
                             <View style={styles.main}>
-                                <Image style={styles.imageView} source={require('../../image/post/diary.png')}/>
+                                <Image style={styles.imageView} source={require('../../image/post/diary.png')} />
                                 <Text style={styles.nameView}>日记</Text>
                                 <Text style={styles.textView}>记录宠物日常</Text>
                             </View>
@@ -124,7 +124,7 @@ const
             marginLeft: -150
         }
     })
-;
+    ;
 export {
     MessageScreen
 }
